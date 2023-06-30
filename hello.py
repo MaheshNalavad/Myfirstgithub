4016cd06ee43c77e35b0dd8b0ad5403804013b27
MaheshNalavad <138227409+MaheshNalavad@users.noreply.github.com>
2023-06-30 19:59:10 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,60 @@
+# List Program
+list=["Akhilesh","mahesh"]
+print(list)
+list=["saurabh","samir","tushr","arvind"]
+print(list)	
+list=["1","2","3"]	
+print(list)
+list=["sona","3","mahi","5"]
+print(list)
+# Length find #
+a=[1,2,3,4,5]
+print(len(a))
+b=[1,2,3,4,5,6,7,8,9,10,12,13,56]
+print(len(b))
+c=["mahesh"]
+print(len(c))
+d=['m','a','h','e','s','h']
+print(len(d))
+#insert element
+list = [1, 2, 3, 4]
+list.append(5)
+print(list)
+v =[12,54,543,4]
+v.append(90)
+print(v)
+d=[7.8,5,2,9,1,4]
+d.append(25)
+print(d)
+# add the two list
+list = [1, 2, 3]
+list1 = [4, 5]
+list.extend(list1)
+print(list)   
+k = [5,8,8,9]
+k1 = [3,2,4,5,1,6]
+k.extend(k1)
+print(k)
+
+m = [1,5,7,0,6]
+m1 = [6,3,2,1,0]
+m.extend(m1)
+print(m)
+
+f = ['m','a','h','e']
+f1 = ['v','i','s','h']
+f.extend(f1)
+print(f)
+
+z = ['m','a','h','e']
+z1 = [1,2,3,4,5]
+z.extend(z1)
+print(z)	
+# chang the length of array
+r = [1,9,7,2]
+r[2] = 12
+print(r)
+ 
+v= [8,5,0,3]
+v[3] = 18
+print(v)
