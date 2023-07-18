1194b3b44d052273c5bab96e850d53787e9e6449
MaheshNalavad <138227409+MaheshNalavad@users.noreply.github.com>
2023-07-18 19:11:36 +0530

    Add files via upload

---
+++ m.py
@@ -0,0 +1,103 @@
+def Mahesh(name,age):
+	print('hii', name)
+	print('How old are you', age)
+Mahesh("sona",28)
+
+num1=25
+num2=30
+def add_number(num1 , num2):
+	result = num1+num2
+	print('The sum is', result)
+	
+add_number(num1,num2)
+
+y=200
+def add(y): 
+	x=100
+	c=200
+	d= x+y+c
+	print(d)
+add(y)
+
+def Mahesh():
+	name='Infosys Solution'
+	print('welcome to', name)
+Mahesh()
+
+def add():
+	x=20000
+	y=3054
+	c=x+y
+	print(c)
+add()
+def sub():
+	x=5000
+	y=200
+	c=x-y
+	if x>y:
+		print('sub of c is ',c)
+		
+	else:
+		x<y
+		print('empty')
+sub()
+
+def cal():
+	x=10000
+	y=20000
+	c=x+y
+	print(c)
+	
+	x=5000
+	y=200
+	c=x-y
+	print(c)
+cal()
+
+x=10000
+def calculator(x,y):
+	#x=10000
+	y=20000
+	c=x+y
+	z=x*y
+	a=x/y
+	print(c)
+	print(z)
+	print(a)
+calculator(x,y)
+
+a=10
+def custom(b,c):
+	d= a+b+c 
+	print(d)
+custom(10,20)
+def child(*son):
+	print('the yungist son is '+ son[6][0])
+child('Arvind','Mahsh','Sonali','Sun','rahul','rohit','saurabh',['Manoj','rushikesh','shubham'])
+
+boys=""
+def CN_Technology(boys1,boys2,boys3):
+	print('the fevorite boys is ' + boys2 )
+	if boys != boys2:
+		print('rest are useless') 
+CN_Technology(boys1='tushar', boys2='Sujit', boys3 ='sona')
+
+def my_function(lname):
+  print(lname + " nalavade")
+my_function("samir")
+my_function("saurabh")
+my_function("rohan")
+my_function("Mahesh & ")
+
+
+def function(list):
+  for i in list:
+    print(i)
+employye=["Vishnu", "Mahesh","Sujit"]
+employye.append("Sonali")
+#employye.remove("Savita")
+employye.insert(2,"Mahi")
+print(employye[3])
+print(employye)   
+
+  
